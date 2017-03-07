3195676fb70294217405ae7471e545c9786d9eff
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-03-07 09:44:54 -0800

    v3d-mmu: adding build and device setup to support enabling mmu.
    refs #SWANDROID-3702
    
    Change-Id: I6dc33600dd099410e6f772e4f7a64c11d5a80e54
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- settings.mk
+++ settings.mk
@@ -91,6 +91,7 @@ export HW_TZ_SUPPORT                         ?= n
 export HW_AB_UPDATE_SUPPORT                  ?= n
 export HW_WIFI_NIC_DUAL_SUPPORT              ?= n
 export HAL_HWC_VERSION                       ?= v-2.0
+export HW_GPU_MMU_SUPPORT                    ?= n
 
 export BCM_GPT_CONFIG_FILE                   := $(LOCAL_DEVICE_GPT)
 export ANDROID_BUILD                         := y
