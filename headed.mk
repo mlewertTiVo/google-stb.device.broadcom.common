fff2966772bbaa738a44dd323a19845dc81b6254
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-01-26 15:51:53 -0800

    common: revise nexus 'debug' settings.
    it turns out that nexus 'debug' settings are more than debug, they also
    are used to enforce nexus object validation which is a good feature to have.
    
    without this, trying to pass invalid object to nexus can have unpredictable
    consequences, when running nexus 'user' it may simply lead to user process
    crash, however when runnig nexus 'kernel' it can lead to invalid pointers
    being accessed which may cause kernel crash subsequently, this is not really
    desirable.
    
    in particular, during system shutdown (such as forced reboot), as we cannot
    guarantee the order of shutdown; if nexus has gone first and invalidated all
    resources allocated by any process, as other process also shut down, not
    validating resource handle would potentially lead to invalid pointer dereference.
    
    to avoid getting in such situation, we re-tune the nexus 'debug' settings for
    -user build to allow object validation enforcement and minimal logging (as
    previously desired in such mode).
    
    refs #SWANDROID-4760
    
    Change-Id: I2109485d0f1cde82185d5eb8f218ed488757efab
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>
    (cherry picked from commit ebdc625f7b879def6b275285ebb3cae18b55d81e)

--- headed.mk
+++ headed.mk
@@ -41,11 +41,10 @@ endif
 include device/broadcom/common/middleware/definitions.mk
 
 ifneq ($(BCM_DIST_KNLIMG_BINS),y)
-ifeq ($(TARGET_BUILD_VARIANT),user)
-export B_REFSW_DEBUG       ?= n
-export B_REFSW_DEBUG_LEVEL :=
-else
 export B_REFSW_DEBUG       ?= y
+ifeq ($(TARGET_BUILD_VARIANT),user)
+export B_REFSW_DEBUG_LEVEL := wrn
+else
 export B_REFSW_DEBUG_LEVEL := msg
 export NEXUS_EXTRA_CFLAGS  := -DNEXUS_P_TRACE=1 -DNEXUS_P_SYNC_TRACE=1
 endif
