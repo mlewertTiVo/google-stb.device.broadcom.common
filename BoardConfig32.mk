af4f35790de2222cbd285014d9b7c0deeee50081
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-07-23 10:22:35 -0700

    device: all targets must use 64bit binder.
    refs #SWANDROID-5367
    
    Change-Id: I6cd3f069094cff4c814735fdfd39a3eb3256a395
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- BoardConfig32.mk
+++ BoardConfig32.mk
@@ -5,7 +5,4 @@ TARGET_CPU_ABI          := armeabi-v7a
 TARGET_CPU_ABI2         := armeabi
 TARGET_CPU_SMP          := true
 
-ifeq ($(LOCAL_ARM_AARCH64_COMPAT_32_BIT),y)
-# 32bit user mode with 64bit kernel.
 TARGET_USES_64_BIT_BINDER := true
-endif
