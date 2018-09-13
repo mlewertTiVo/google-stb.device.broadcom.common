423f332977a3daa398a140f2f21e8c1c646c6169
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-09-13 05:32:58 -0700

    device: for binary distribution images, pull dtbo from device root.
    refs #SWANDROID-5559
    
    Change-Id: Iffdaf89db99889e34882836a50e642054fcedf2a
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- AndroidPhony.mk
+++ AndroidPhony.mk
@@ -1,13 +1,9 @@
 .PHONY: bootloader.img
-#bootloader.img: blimg
 .PHONY: bootloader.dev.img
 .PHONY: bootloader.prod.img
+.PHONY: dtbo.img
 
 ifneq ($(BCM_DIST_BLIMG_BINS),y)
 .PHONY: gpt.bin
 gpt.bin: makegpt
 endif
-
-.PHONY: dtbo.img
-dtbo.img: dtboimg
-
