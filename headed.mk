4e76cabc077d25aade00a062c467b4d3d3a85fce
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-09-28 06:06:25 -0700

    device: runtime resources overlay (rro) for frameworks package.
    refs #SWANDROID-5593
    
    Change-Id: I67d2c9d83737098998de619c40ded22e86602f34
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- headed.mk
+++ headed.mk
@@ -533,7 +533,8 @@ endif
 
 PRODUCT_PACKAGES += \
     BcmCustomizer \
-    BcmPlayAutoInstallConfig
+    BcmPlayAutoInstallConfig \
+    BcmFwksRro
 
 ifneq ($(filter $(ANDROID_SUPPORTS_WIDEVINE) $(ANDROID_SUPPORTS_PLAYREADY),y),)
 PRODUCT_PROPERTY_OVERRIDES  += drm.service.enabled=true
