c81ee038d36dc6500737be3e1f771e760aea401c
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-10-25 10:02:35 +0100

    hals: add casproxy policy needed for some cas integrations
    note: allowing cas-proxy access to core socket is a violation
          of certification, it can be done for testing but cannot
          be used as a final product solution.
    
    refs #SWANDROID-5734
    
    Change-Id: I9605edd79774de88240d6cc97af167c36ccfac98
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- config.fs
+++ config.fs
@@ -9,3 +9,9 @@ mode: 0755
 user: AID_WIFI
 group: AID_WIFI
 caps: NET_ADMIN NET_RAW
+
+[system/vendor/bin/hw/bcm.hardware.casproxy@1.0-service]
+mode: 0777
+user: AID_MEDIA
+group: AID_MEDIA
+caps: NET_ADMIN NET_RAW NET_BIND_SERVICE
