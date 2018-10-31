8b6724cd95177e602d088675cb750aff90f9a956
Pierre Couillaud <pierre@broadcom.com>
2018-10-30 19:06:27 -0700

    common: clean up some late additions to hal-manifests.
    breaks P device compatibility...  tvisvcext is not ready for
    primetime so should not appear, sfhak should be unconditional.
    
    refs #SWANDROID-5326
    
    Change-Id: I356b593ccc50c0e4bd109e546720a8fecd5f33cd
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- headed.mk
+++ headed.mk
@@ -454,11 +454,8 @@ PRODUCT_PACKAGES += \
    bcm.hardware.dspsvcext-V1.0-java \
    bcm.hardware.sdbhak@1.0-service \
    bcm.hardware.tvisvcext@1.0-service \
-   bcm.hardware.dpthak@1.0-service
-ifeq ($(ANDROID_SUPPORTS_MEDIACAS),y)
-PRODUCT_PACKAGES += \
+   bcm.hardware.dpthak@1.0-service \
    bcm.hardware.sfhak@1.0-service
-endif
 ifeq ($(ANDROID_SUPPORTS_WIDEVINE),y)
 PRODUCT_PACKAGES += \
    android.hardware.drm@1.1-service.widevine
