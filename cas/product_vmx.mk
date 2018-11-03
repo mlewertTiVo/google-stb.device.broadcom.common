b911a758a1f426faf2016b02c5efb84573ca5811
Rob McConnell <robertwm@broadcom.com>
2018-11-03 16:12:56 +0000

    cas: don't default to prebuilt bootloader & boot binaries
    This change (1 of 2) ensures that by default prebuilt BOLT, BSU,
    bootloader and boot images are not enabled for customers/OEM's builds.
    There will not be an attempt to copy prebuilt binaries from the
    internal Broadcom bindist directory to the product output directory.
    
    For Broadcom internal use, prebuilt binaries will be enabled and
    copied to the product output directory.
    
    refs #SWANDROID-5769
    
    Change-Id: I24c025590a35124119ac746dd8c7c450563a2951
    Signed-off-by: Rob McConnell <robert.mcconnell@broadcom.com>

--- cas/product_vmx.mk
+++ cas/product_vmx.mk
@@ -24,20 +24,6 @@ export SAGE_BINARY_EXT :=
 export LOCAL_DEVICE_SECURITY_VARIANT_DEV ?= up
 export LOCAL_DEVICE_SECURITY_VARIANT_PROD ?= up
 
-# Binary distribution of bolt, BSU, bootloader and boot images
-export BCM_BINDIST_BL_ROOT  := vendor/broadcom/bindist/blimg/vmx/${LOCAL_PRODUCT_OUT}
-
-# Comment out the line below to disable pre-built boot images
-export BCM_DIST_BLIMG_BINS  := y
-
-# Copy over the signed AVB bolt, AVB BSU and boot images (if they exist)
-ifneq ($(wildcard ${TOP}/${BCM_BINDIST_BL_ROOT}/boot-signed.img),)
-PRODUCT_COPY_FILES += \
-    ${BCM_BINDIST_BL_ROOT}/android_bsu-vb.signed.elf:$(PRODUCT_OUT_FROM_TOP)/android_bsu-vb.signed.elf \
-    ${BCM_BINDIST_BL_ROOT}/bolt-vb.signed.bin:$(PRODUCT_OUT_FROM_TOP)/bolt-vb.signed.bin \
-    ${BCM_BINDIST_BL_ROOT}/boot-signed.img:$(PRODUCT_OUT_FROM_TOP)/boot-signed.img
-endif
-
 # Copy over the VMX SAGE TA
 PRODUCT_COPY_FILES += \
     ${VENDOR_VMX_PRODUCT_ROOT}/TA/SDL_TA_verimatrix_ultra.bin:$(TARGET_COPY_OUT_VENDOR)/bin/SDL_TA_verimatrix_ultra.bin
