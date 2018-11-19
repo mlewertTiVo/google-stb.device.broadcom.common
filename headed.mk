1bb661e01680bf86399cce5de4c8514ac724a92e
Edwin Chan <edwin.chan@broadcom.com>
2018-11-19 11:54:08 -0800

    netflix: upgrade to NRDP 6 validation
    refs #SWANDROID-5293
    
    Change-Id: I79d93ac69aabef1824614e9e9a59e5b752e102bf
    Signed-off-by: Edwin Chan <edwin.chan@broadcom.com>

--- headed.mk
+++ headed.mk
@@ -576,7 +576,7 @@ endif
 
 # Netflix support
 ifneq (${LOCAL_DEVICE_NRDP_MODEL_GROUP},)
-LOCAL_DEVICE_NRDP_VALIDATION ?= ninja_5.1
+LOCAL_DEVICE_NRDP_VALIDATION ?= ninja_6
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.vendor.nrdp.modelgroup=${LOCAL_DEVICE_NRDP_MODEL_GROUP} \
    ro.vendor.nrdp.validation=${LOCAL_DEVICE_NRDP_VALIDATION}
