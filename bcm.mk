7fdc108ad5892279f7de3a5b56abb83f05e5de0d
Luis Navarro <luis.navarro@broadcom.com>
2018-12-12 16:39:36 -0800

    common: enable tunneling hfr
    refs #SWANDROID-5879
    
    Change-Id: Ia38dec97002508c53b5a11094fe931f00236d264
    Signed-off-by: Luis Navarro <luis.navarro@broadcom.com>
    (cherry picked from commit 35b9746fcc0e3e3a58788e81aab114a3bb8ca1e2)

--- bcm.mk
+++ bcm.mk
@@ -17,6 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
    ro.nx.colordepth10b.force=1 \
    \
    ro.nx.media.vdec_outportbuf=32
+   \
+   ro.nx.media.vdec_hfrvideo_tunnel=1
+
 #
 # ro.nx.pr.version defaults to 2.5
 #
