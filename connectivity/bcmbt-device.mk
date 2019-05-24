ef21530a9c3d2f7bf13f1258f505a7c45241e765
Blair Prescott <Blair.Prescott@broadcom.com>
2019-05-24 15:33:01 -0700

    common: Use updated BT firmware for BCM43570
    Use Bluetooth firmware addressing security vulnerability CVE-2018-19860.
    Clean up unused chip definitions.
    
    refs #SWANDROID-6342
    refs #SWANDROID-6410
    
    Change-Id: I462d7c34c4f92c234bfa0a8f8fc2471aa4d72770
    (cherry picked from commit b338bac29645b603a2044707ec1ca0368a8e5cef)
    (cherry picked from commit f7f00ae1bcd6d9a73ac4aedc65e3f7b8646058e3)

--- connectivity/bcmbt-device.mk
+++ connectivity/bcmbt-device.mk
@@ -2,19 +2,9 @@
 
 ifeq ($(ANDROID_ENABLE_BT),usb)
 
-ifeq ($(BROADCOM_WIFI_CHIPSET),43242a1)
-PRODUCT_COPY_FILES += \
-    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43242A1_001.002.007.0077.0000_Generic_USB_Class2_Generic_3DTV_TBFC_37_4MHz_Wake_on_BLE_Hisense.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43242.hcd
-endif
-
-ifeq ($(BROADCOM_WIFI_CHIPSET),43570a0)
-PRODUCT_COPY_FILES += \
-    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43569A0_001.001.009.0039.0000_Generic_USB_40MHz_fcbga_BU_TXPwr_8dbm_4.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
-endif
-
 ifeq ($(BROADCOM_WIFI_CHIPSET),43570a2)
 PRODUCT_COPY_FILES += \
-    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43569A2_001.003.004.0148.0000_Generic_USB_40MHz_fcbga_BU_WakeOn_BLE_Google_BT4_2.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
+    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43569A2_001.003.004.0169.0000_Generic_USB_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
 endif
 
 else # BT UART
@@ -30,8 +20,8 @@ PRODUCT_COPY_FILES += device/broadcom/common/rcs/init.bcm.bt.rc:$(TARGET_COPY_OU
 else
 PRODUCT_COPY_FILES += device/broadcom/common/rcs/init.bcm.bt.rc:root/init.brcm_bt.rc
 endif
-ifeq ($(BROADCOM_WIFI_CHIPSET),43570a0)
-PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/BCM43569A2_001.003.004.0154.0000_Generic_UART_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
+ifeq ($(BROADCOM_WIFI_CHIPSET),43570a2)
+PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/BCM43569A2_001.003.004.0169.0000_Generic_UART_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
 else
 PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/CYW20703A1_001.001.005.0401.0805.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM20703.hcd
 endif
