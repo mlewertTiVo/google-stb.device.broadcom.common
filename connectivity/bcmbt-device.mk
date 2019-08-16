9ee0319e5bf289e9c34fcbb48caab33e13840bc5
Blair Prescott <Blair.Prescott@broadcom.com>
2019-08-15 17:01:36 -0700

    common: Use updated BT firmware for BCM43570
    Use Bluetooth firmware addressing security vulnerability CVE-2019-11516.
    
    refs #SWANDROID-6698
    refs #SWANDROID-6876
    
    Change-Id: I7dea91a4aaa3dbaa5d9379cef7a890fa6467a49d
    (cherry picked from commit a9508d6d10f67df00ff21f7ff3252378da4eba58)
    (cherry picked from commit a59fd38b602775c051a5bc2e59e7092b3ef8a5e3)

--- connectivity/bcmbt-device.mk
+++ connectivity/bcmbt-device.mk
@@ -4,7 +4,7 @@ ifeq ($(ANDROID_ENABLE_BT),usb)
 
 ifeq ($(BROADCOM_WIFI_CHIPSET),43570a2)
 PRODUCT_COPY_FILES += \
-    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43569A2_001.003.004.0169.0000_Generic_USB_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
+    ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btusb/firmware/BCM43569A2_001.003.004.0172.0000_Generic_USB_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
 endif
 
 else # BT UART
@@ -21,7 +21,7 @@ else
 PRODUCT_COPY_FILES += device/broadcom/common/rcs/init.bcm.bt.rc:root/init.brcm_bt.rc
 endif
 ifeq ($(BROADCOM_WIFI_CHIPSET),43570a2)
-PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/BCM43569A2_001.003.004.0169.0000_Generic_UART_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
+PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/BCM43569A2_001.003.004.0172.0000_Generic_UART_40MHz_fcbga_BU.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43569.hcd
 PRODUCT_COPY_FILES += ${BCM_VENDOR_STB_ROOT}/bcm_platform/conx/btuart/firmware/CYW20703A1_001.001.005.0401.0805.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM20703.hcd
 endif
 
