8eb1f4c5bfd1e6382f66f6664f04b50421a61dd0
Pierre Couillaud <pierre@broadcom.com>
2020-01-21 04:19:13 -0800

    vsp: push data forward to latest compliance
    refs #SWANDROID-7575 (CLONE)
    refs #SWANDROID-7431
    
    Change-Id: I6fa171552bad542eb0dfd6ae660cd442d2f1a3e9
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    (cherry picked from commit f9b177d1528cbffae90139142741de0f4255aa42)
    (cherry picked from commit dc2ae284805b7c1e1fb50818543aae365461f753)

--- vsp.mk
+++ vsp.mk
@@ -1,4 +1,4 @@
 # currently enforced security path level for /vendor, includes
 # kernel patch level and all vendor only components (wifi, bt, etc)
 
-VENDOR_SECURITY_PATCH := 2019-12-05
+VENDOR_SECURITY_PATCH := 2020-01-05
